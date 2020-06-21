a53c92bbe7334848e8219bd7e89ae8474e3db9af
Mathew Davidov <41017026+MathewDavidov@users.noreply.github.com>
2020-06-20 23:47:54 -0400

    Update allFavoriteMovies.js

--- src/store/utilities/allFavoriteMovies.js
+++ src/store/utilities/allFavoriteMovies.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = "https://cuny-capstone-server.herokuapp.com";
 
 // ACTION TYPES
 const FETCH_FAVORITE_MOVIES = "FETCH_FAVORITE_MOVIES";
