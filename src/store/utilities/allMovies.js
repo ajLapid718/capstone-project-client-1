25f2f058199cfd9529421d0e05c6de995eb3f4a3
Mathew Davidov <41017026+MathewDavidov@users.noreply.github.com>
2020-06-20 23:41:08 -0400

    Update allMovies.js

--- src/store/utilities/allMovies.js
+++ src/store/utilities/allMovies.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = "https://cuny-capstone-server.herokuapp.com/";
 
 // ACTION TYPES
 const FETCH_ALL_MOVIES = "FETCH_ALL_MOVIES";
